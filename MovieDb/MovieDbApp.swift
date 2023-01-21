e47d2e13d36ded55eb8153bc31f53c4393a30530
Chirag Arora <arora2002chirag@gmail.com>
2023-01-21 11:07:04 +0530

    Initial Commit

---
+++ MovieDb/MovieDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDbApp.swift
+//  MovieDb
+//
+//  Created by chirag arora on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
